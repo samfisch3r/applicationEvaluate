974ac140d7eadb0843026420af611ca71d38e4e0
samfisch3r <sam@dienachbarn.ch>
2017-04-17 23:08:54 +0200

    add initial functionality

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var stylus = require('stylus');
+var nib = require('nib');
+var busboy = require('connect-busboy');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .set('compress', true)
+    .use(nib());
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,15 +25,18 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(busboy());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(stylus.middleware(path.join(__dirname, 'public')));
+app.use(stylus.middleware({
+  src: __dirname + '/public',
+  compile: compile
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,80 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var mongojs = require('mongojs');
+var csv = require('csvtojson');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var databaseURI = 'localhost:27017/valuation';
+var collections = ['applications', 'criteria', 'judge'];
+var db = mongojs(databaseURI, collections);
+
+router.route('/')
+	.get(function(req, res, next) {
+		res.render('index', {
+			title: 'Application Evaluate'
+		});
+	})
+	.post(function(req, res, next) {
+		req.pipe(req.busboy);
+		req.busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
+			if (mimetype == 'text/csv') {
+				var fstream = fs.createWriteStream('./public/upload.csv');
+				file.pipe(fstream);
+			}
+			file.on('error', function(err) {
+				console.log(err);
+			});
+		});
+		req.busboy.on('error', function(err){
+			console.log(err);
+		});
+		req.busboy.on('finish', function() {
+			csv({
+				delimiter: ';'
+			})
+			.fromFile('./public/upload.csv')
+			.on('json',(jsonObj)=>{
+				addUserData(jsonObj);
+			})
+			.on('done',()=>{
+				console.log('end');
+			});
+			res.redirect('back');
+		});
+	});
+
+router.route('/judgeoverview')
+	.get(function(req, res, next) {
+		res.render('judgeoverview', {
+			title: 'Evaluation Criteria Overview'
+		});
+	});
+
+router.route('/judge')
+	.get(function(req, res, next) {
+		res.render('judge', {
+			title: 'Evaluation Criteria'
+		});
+	});
+
+router.route('/applicationdata')
+	.get(function(req, res, next) {
+		res.render('applicationdata', {
+			title: 'Application Data'
+		});
+	});
+
+router.route('/applicationjudge')
+	.get(function(req, res, next) {
+		res.render('applicationjudge', {
+			title: 'Application Data Evaluation'
+		});
+	});
+
+function addUserData(data) {
+	db.applications.insert(data, function(err, doc) {
+		if(err) throw err;
+	});
+}
 
 module.exports = router;
